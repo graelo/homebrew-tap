070b599875e271d88a3e29482b60724d350b7790
graelo <84066822+graelo@users.noreply.github.com>
2022-08-24 19:49:38 +0000

    sshpass: add 1.08 bottle.

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -4,6 +4,12 @@ class Sshpass < Formula
   url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.08/sshpass-1.08.tar.gz"
   sha256 "8bdacb8ca4ecf82c84a5effa2f75a8bf4ed9dd6f9d030f308fe11cffb3830b02"
 
+  bottle do
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/sshpass-1.08"
+    sha256 cellar: :any_skip_relocation, big_sur:      "d7f867910f7711d8d7fa6e7462c0dbc17c1fd49ca6349c5ee0395907eb4e998c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "13c2b24a85896ea09ca2451f724350de8d275f4805382bf91b5f584d53c8680d"
+  end
+
   depends_on "gnu-sed"
 
   def install
