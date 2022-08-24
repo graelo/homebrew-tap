ed95fd6e99e72accef86c27bbc600e61a38e9c7b
graelo <graelo@grael.cc>
2022-08-24 21:38:36 +0200

    sshpass 1.08

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,7 +1,7 @@
 class Sshpass < Formula
   desc "Pass a password to ssh (insecure but practical)"
   homepage "https://sourceforge.net/projects/sshpass"
-  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.06/sshpass-1.06.tar.gz"
+  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.08/sshpass-1.08.tar.gz"
   sha256 "8bdacb8ca4ecf82c84a5effa2f75a8bf4ed9dd6f9d030f308fe11cffb3830b02"
 
   depends_on "gnu-sed"
