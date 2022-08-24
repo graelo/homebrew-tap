47054c6b6203d5c192f660fb9665da2cea448577
graelo <graelo@grael.cc>
2022-08-24 22:17:33 +0200

    sshpass-1.08: add macOS ventura bottle

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -6,8 +6,9 @@ class Sshpass < Formula
 
   bottle do
     root_url "https://github.com/graelo/homebrew-tap/releases/download/sshpass-1.08"
-    sha256 cellar: :any_skip_relocation, big_sur:      "d7f867910f7711d8d7fa6e7462c0dbc17c1fd49ca6349c5ee0395907eb4e998c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "13c2b24a85896ea09ca2451f724350de8d275f4805382bf91b5f584d53c8680d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "77672237ec21ee89706194e1016babad29c46fe09dd4be8b730c0262080b045b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d7f867910f7711d8d7fa6e7462c0dbc17c1fd49ca6349c5ee0395907eb4e998c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13c2b24a85896ea09ca2451f724350de8d275f4805382bf91b5f584d53c8680d"
   end
 
   depends_on "gnu-sed"
