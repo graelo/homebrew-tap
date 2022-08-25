de9e3b977edb23f3d25c7566c5d1abe3e254a496
graelo <graelo@grael.cc>
2022-08-25 14:07:41 +0200

    sshpass 1.09: add macOS ventura bottle

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -6,9 +6,10 @@ class Sshpass < Formula
 
   bottle do
     root_url "https://github.com/graelo/homebrew-tap/releases/download/sshpass-1.09"
-    sha256 cellar: :any_skip_relocation, monterey:     "b9f6661c44e8969f8ad55d7dfbc22384855f9e94a3b52fe0133c3b4e4dd07275"
-    sha256 cellar: :any_skip_relocation, big_sur:      "04932de6f96980165c66f26aebac21d55cbe1052def35e71c34742b961dcc321"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "8b3bc84b0a189f459a8d9bf7c8a593b80f2c3d08fd2cb8da74872150301a5fdb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d2c5767719fd9b8777dd889fe4032fdbafe6b2c195dc9acf93125db55ca1a30e"
+    sha256 cellar: :any_skip_relocation, monterey:      "b9f6661c44e8969f8ad55d7dfbc22384855f9e94a3b52fe0133c3b4e4dd07275"
+    sha256 cellar: :any_skip_relocation, big_sur:       "04932de6f96980165c66f26aebac21d55cbe1052def35e71c34742b961dcc321"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8b3bc84b0a189f459a8d9bf7c8a593b80f2c3d08fd2cb8da74872150301a5fdb"
   end
 
   depends_on "gnu-sed"
