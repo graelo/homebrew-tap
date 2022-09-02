04e0d6ecb5373950e9f93cf816f714a24cf49d6c
graelo <graelo@grael.cc>
2022-09-03 00:13:58 +0200

    jsonnet-language-server 0.7.0

---
+++ Formula/jsonnet-language-server.rb
@@ -0,0 +1,17 @@
+class JsonnetLanguageServer < Formula
+  desc "Language Server Protocol (LSP) server for Jsonnet (https://jsonnet.org)"
+  homepage "https://github.com/grafana/jsonnet-language-server"
+  url "https://github.com/grafana/jsonnet-language-server/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "2e9a92214d8e0695dda95c063e329415e8f47eff8fe2bcfe457b25bf48dbdc4d"
+  license "AGPL-3.0-only"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    assert_match "jsonnet-language-server", shell_output("#{bin}/jsonnet-language-server --version")
+  end
+end
