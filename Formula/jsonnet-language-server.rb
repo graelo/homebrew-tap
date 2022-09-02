85c9bdb932b143cc1b13bb669911fb89d5d90650
graelo <graelo@grael.cc>
2022-09-03 00:20:11 +0200

    jsonnet-language-server 0.8.0

--- Formula/jsonnet-language-server.rb
+++ Formula/jsonnet-language-server.rb
@@ -1,7 +1,7 @@
 class JsonnetLanguageServer < Formula
   desc "Language Server Protocol (LSP) server for Jsonnet (https://jsonnet.org)"
   homepage "https://github.com/grafana/jsonnet-language-server"
-  url "https://github.com/grafana/jsonnet-language-server/archive/refs/tags/v0.7.0.tar.gz"
+  url "https://github.com/grafana/jsonnet-language-server/archive/refs/tags/v0.8.0.tar.gz"
   sha256 "2e9a92214d8e0695dda95c063e329415e8f47eff8fe2bcfe457b25bf48dbdc4d"
   license "AGPL-3.0-only"
 
