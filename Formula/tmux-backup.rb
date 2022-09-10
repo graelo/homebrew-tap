651a82f79afad5e3ea0d5c13e795c5002a20eb5a
graelo <84066822+graelo@users.noreply.github.com>
2022-09-10 18:27:54 +0000

    tmux-backup: update 0.1.1 bottle.

--- Formula/tmux-backup.rb
+++ Formula/tmux-backup.rb
@@ -6,10 +6,9 @@ class TmuxBackup < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/graelo/homebrew-tap/releases/download/tmux-backup-0.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ad177a80e25a5fde8d524bac10dabd988aaa0c58260d43dd45b0bb86b0a554f6"
-    sha256 cellar: :any_skip_relocation, monterey:      "c6acfeaf12d9bf0c16da46e0f5612ee3a3cdc065c6eec42e7422f0293d23d923"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7e865749883ae06063ca97b8ad4153b264c113d9e835ade407d1fbd909bb7f54"
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/tmux-backup-0.1.1"
+    sha256 cellar: :any_skip_relocation, monterey: "6dc2bd2a95541a7541c9976cb8d9fd840914ae8a434a024ac476f641e81e5fa6"
+    sha256 cellar: :any_skip_relocation, big_sur:  "f506deb2dac349873560ca5eba38417a99397e486a321064e11b439a6688ef39"
   end
 
   depends_on "rust" => :build
