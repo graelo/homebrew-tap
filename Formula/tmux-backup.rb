957c5261668e7d8c23e7be75c7f8c7c2c4ceeaed
graelo <graelo@grael.cc>
2022-10-31 00:03:17 +0100

    tmux-backup 0.3.1: add macOS ventura bottle

--- Formula/tmux-backup.rb
+++ Formula/tmux-backup.rb
@@ -7,9 +7,10 @@ class TmuxBackup < Formula
 
   bottle do
     root_url "https://github.com/graelo/homebrew-tap/releases/download/tmux-backup-0.3.1"
-    sha256 cellar: :any_skip_relocation, monterey:     "697437bb5e4a322e7a093dea2e26d83e12bddc71948851be9122f9ea1f9573e9"
-    sha256 cellar: :any_skip_relocation, big_sur:      "6ac8a1a3829b4d38ff70f34294af610743354ccbca684ca3c71a4c541e9c2a79"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "40ec74cb86206f5127e71dae29c6051c4e6fa54b8519a0bdfbbd7f3fa205ed00"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2ed44d27fe17482518aaeb805351ff56d07e795403dbf0cb52059034a2c84ee1"
+    sha256 cellar: :any_skip_relocation, monterey:      "697437bb5e4a322e7a093dea2e26d83e12bddc71948851be9122f9ea1f9573e9"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6ac8a1a3829b4d38ff70f34294af610743354ccbca684ca3c71a4c541e9c2a79"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "40ec74cb86206f5127e71dae29c6051c4e6fa54b8519a0bdfbbd7f3fa205ed00"
   end
 
   depends_on "rust" => :build
