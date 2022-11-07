55f953dc823c76b92e23fbdc82bfd185f04d2b40
graelo <graelo@grael.cc>
2022-11-07 16:57:57 +0100

    tmux-copyrat 0.4.1

---
+++ Formula/tmux-copyrat.rb
@@ -0,0 +1,17 @@
+class TmuxCopyrat < Formula
+  desc "tmux-copycat on Rust steroids"
+  homepage ""
+  url "https://github.com/graelo/tmux-copyrat/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "943c8a9d88f414df5b41870ab82bd0d67237897d495a0da25d9d1224cd3ebf5d"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "true"
+  end
+end
