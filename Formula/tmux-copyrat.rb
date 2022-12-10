8d2643ff6b8d6d5a41ff1a4b6af0902ecb7acd49
graelo <84066822+graelo@users.noreply.github.com>
2022-12-10 16:13:18 +0000

    tmux-copyrat: update 0.5.1 bottle.

--- Formula/tmux-copyrat.rb
+++ Formula/tmux-copyrat.rb
@@ -6,11 +6,10 @@ class TmuxCopyrat < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/graelo/homebrew-tap/releases/download/tmux-copyrat-0.5.0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "28b3bca1a5dff080456bf56eccfe01d25c252140bec6527ce7c0c346a54162e6"
-    sha256 cellar: :any_skip_relocation, monterey:      "dba26115cee3217a62325d9d673b1a32118a5a7a1ef8f239a4a0bf6180a6faad"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9ec2571b69d62d5be3d6f15fa979184a13eea6aafc6e2b4190fa51c4588a9a1d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "210e7bcbfeddb7ff8ee7dba3fea160b5ca4faac01e62f5986c12391a28a1c76f"
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/tmux-copyrat-0.5.1"
+    sha256 cellar: :any_skip_relocation, monterey:     "9d1e60f8a39cc32834b2ce28c47f5e1f47700b7e5de9ca0dca7f6db0d34211cd"
+    sha256 cellar: :any_skip_relocation, big_sur:      "a48c001d1a06c7c6d6833b969ec172dee4525791ca46ad62a4c7e44c13234fd3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "bb852980d13b21b839bd65f8058795e94e4f36b03b760e950049244063a79c1b"
   end
 
   depends_on "rust" => :build
