74a9f2ef87b0fb4fb0c85599391c025c18194f01
graelo <84066822+graelo@users.noreply.github.com>
2023-04-02 08:06:16 +0000

    podsync: add 0.0.7 bottle.

--- Formula/podsync.rb
+++ Formula/podsync.rb
@@ -5,6 +5,12 @@ class Podsync < Formula
   sha256 "33a29bd444d4e7e70ee17d3783d73964adea0ed57bf422239da5b3dc32804e6a"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/podsync-0.0.7"
+    sha256 cellar: :any_skip_relocation, monterey:     "653a3bdb81cdbfff734da82440aa71401ad33a7f3a9c0180cb62ccce8db0970f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2c487c420916d23f6b87a4ea31079f0428b5abc00512c0df3d848369adc305f8"
+  end
+
   depends_on "rust" => [:build, :test] unless system "command", "--query", "cargo"
 
   def install
