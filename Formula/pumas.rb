d3b0d0810f116b7259f7141c56beb8b1876a0423
graelo <graelo@grael.cc>
2023-04-08 23:30:52 +0200

    add pumas

---
+++ Formula/pumas.rb
@@ -0,0 +1,18 @@
+class Pumas < Formula
+  desc "Power Usage Monitor for Apple Silicon"
+  homepage "https://github.com/graelo/pumas"
+  url "https://github.com/graelo/pumas/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "903ca5a1f7e56f6d0bb8fcb865c2bdfe76fdbeddd634a1a8bca5c88c97d46564"
+  license ""
+
+  depends_on "rust" => [:build, :test]
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    generate_completions_from_executable(bin/"pumas", "generate-completion")
+  end
+
+  test do
+    assert_match "#compdef pumas", shell_output("#{bin}/pumas generate-completion zsh")
+  end
+end
