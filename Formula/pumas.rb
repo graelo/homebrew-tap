903311b1644e543d7643c0e1b92b9e0491b0be1f
graelo <graelo@grael.cc>
2023-04-08 23:38:10 +0200

    pumas 0.0.1: add macOS ventura bottle

--- Formula/pumas.rb
+++ Formula/pumas.rb
@@ -7,6 +7,10 @@ class Pumas < Formula
 
   depends_on "rust" => [:build, :test]
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a3bc5b845dab2b13b4f104a59623bebdfcafeb6820200753b5a3156d3ed4a012"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
     generate_completions_from_executable(bin/"pumas", "generate-completion")
