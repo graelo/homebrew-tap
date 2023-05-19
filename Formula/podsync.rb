04f7f1d96820417383033a2eb76d056636a836d7
graelo <graelo@grael.cc>
2023-05-19 22:50:50 +0200

    podsync 0.0.8: add macOS ventura bottle

--- Formula/podsync.rb
+++ Formula/podsync.rb
@@ -7,8 +7,9 @@ class Podsync < Formula
 
   bottle do
     root_url "https://github.com/graelo/homebrew-tap/releases/download/podsync-0.0.8"
-    sha256 cellar: :any_skip_relocation, monterey:     "e03e6a9245dc2d77a28d4c903c85ee20a72010e1c5594d24917e732f09c7d57a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "62eeeb295510a781c9873efc82096b9174c91964dbebd46ab42ae8e182011e68"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "263729dacc4ff3f38e78654afb34ec883262b26f60f50df6839dc6ece3b33725"
+    sha256 cellar: :any_skip_relocation, monterey:      "e03e6a9245dc2d77a28d4c903c85ee20a72010e1c5594d24917e732f09c7d57a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "62eeeb295510a781c9873efc82096b9174c91964dbebd46ab42ae8e182011e68"
   end
 
   depends_on "rust" => [:build, :test]
