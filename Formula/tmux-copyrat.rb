bf64409db38885c3b74fe6ee73ae97fcc0bbb990
graelo <graelo@grael.cc>
2023-06-02 23:45:12 +0200

    tmux-copyrat 0.5.3: add macOS ventura bottle

--- Formula/tmux-copyrat.rb
+++ Formula/tmux-copyrat.rb
@@ -7,8 +7,9 @@ class TmuxCopyrat < Formula
 
   bottle do
     root_url "https://github.com/graelo/homebrew-tap/releases/download/tmux-copyrat-0.5.3"
-    sha256 cellar: :any_skip_relocation, monterey:     "171b444d4423860e1117ff559bb11c6bc84b06617879122f996851d28a79a2eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6dff5934f934fbcfb03f30ebb3515de7b6f741d8dc12db74e20493ac93f4ec6e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "76a094d2a0a5e67a13746f1b7ca5f7621d3f3556a1009bfe572ef98c64db4926"
+    sha256 cellar: :any_skip_relocation, monterey:      "171b444d4423860e1117ff559bb11c6bc84b06617879122f996851d28a79a2eb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6dff5934f934fbcfb03f30ebb3515de7b6f741d8dc12db74e20493ac93f4ec6e"
   end
 
   depends_on "rust" => [:build, :test]
