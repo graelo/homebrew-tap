ba34d8ab6a1e2d10a4422136488080ffc659edca
graelo <graelo@grael.cc>
2023-07-23 23:37:32 +0200

    pumas 0.0.7: add macOS ventura bottle

--- Formula/pumas.rb
+++ Formula/pumas.rb
@@ -7,8 +7,7 @@ class Pumas < Formula
 
   bottle do
     root_url "https://github.com/graelo/homebrew-tap/releases/download/pumas-0.0.7"
-    sha256 cellar: :any_skip_relocation, monterey:     "db06e05ac8d13227a194c10712a7359a1807f9236c0e14d7e8f0970931635b55"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "652f6329726d04b58159272075b2bedc0b5401b5f4d211760dd81892e8772c57"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9cdfb258aeedd8179cde3438dc8fef8ae6883864fb922b12a1298d3a2429e53c"
   end
 
   depends_on "rust" => [:build, :test]
