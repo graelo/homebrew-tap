1654808ed2ebd456a16e71e85ed96e636ebd30dc
graelo <84066822+graelo@users.noreply.github.com>
2023-08-03 09:46:50 +0200

    pumas 0.1.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #52.
    
    Signed-off-by: graelo <84066822+graelo@users.noreply.github.com>

--- Formula/pumas.rb
+++ Formula/pumas.rb
@@ -1,8 +1,8 @@
 class Pumas < Formula
   desc "Power Usage Monitor for Apple Silicon"
   homepage "https://github.com/graelo/pumas"
-  url "https://github.com/graelo/pumas/archive/refs/tags/v0.0.11.tar.gz"
-  sha256 "691b3892a3ee9f67bd22b05515eb1a3ba43e44b7b1f57f0af2d6c6bd6699352b"
+  url "https://github.com/graelo/pumas/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "183f66ee16652e8c374d05ef54adc1950ee707c595f56252f59c0fa3e187b738"
   license "MIT"
 
   bottle do
