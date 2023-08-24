df92b2198ec7514a67b9371b4bf399e9cf8942a7
graelo <graelo@grael.cc>
2023-08-25 00:17:22 +0200

    jsonnet-language-server 0.13.0: add macOS ventura bottle

--- Formula/jsonnet-language-server.rb
+++ Formula/jsonnet-language-server.rb
@@ -7,8 +7,9 @@ class JsonnetLanguageServer < Formula
 
   bottle do
     root_url "https://github.com/graelo/homebrew-tap/releases/download/jsonnet-language-server-0.13.0"
-    sha256 cellar: :any_skip_relocation, monterey:     "afa4511540ebbc54432c3a7e36827b19abf71034bf822f2c5d755b4e13329b3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "117f538f97d646f739eb3b9f35a119060bd909b447311ce200109c3306952e3c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "02770ab21f14153cebcddf5965c9eebdcb654798d6abee9b3b26a79ee97f754b"
+    sha256 cellar: :any_skip_relocation, monterey:      "afa4511540ebbc54432c3a7e36827b19abf71034bf822f2c5d755b4e13329b3f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "117f538f97d646f739eb3b9f35a119060bd909b447311ce200109c3306952e3c"
   end
 
   depends_on "go" => [:build, :test]
