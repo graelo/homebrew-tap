7238ec7ebe9fc10a15f499e4caf2d0744b111b41
graelo <84066822+graelo@users.noreply.github.com>
2024-08-07 22:57:05 +0000

    vale-ls: add 0.3.7 bottle.

--- Formula/vale-ls.rb
+++ Formula/vale-ls.rb
@@ -5,6 +5,12 @@ class ValeLs < Formula
   sha256 "3daba73d517a27c1243b85075adff6388cd195294f741ca34fc6f75bd8251ea1"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/vale-ls-0.3.7"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey: "182dc57bfe9906e568276714a7ec903424a7ca76f947b28339f517c7ea3eb6f4"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
 
