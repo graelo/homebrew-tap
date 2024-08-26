1f542bcc374467e24aea25045b545809e7672a0c
graelo <84066822+graelo@users.noreply.github.com>
2024-08-26 14:06:21 +0000

    jsonnet-language-server: update 0.14.1 bottle.

--- Formula/jsonnet-language-server.rb
+++ Formula/jsonnet-language-server.rb
@@ -6,9 +6,10 @@ class JsonnetLanguageServer < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    root_url "https://github.com/graelo/homebrew-tap/releases/download/jsonnet-language-server-0.14.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "1754d168604208f0f5cb9c82e105bb8fa025c3ebda07f3f0c2bb257c378b682f"
-    sha256 cellar: :any_skip_relocation, monterey:     "01d3bdb0e7759f58d9fee76e01c404060b53255b1e81ee76e9d80232538af068"
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/jsonnet-language-server-0.14.1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "c9d2774bc1eba240fa53e3e1ffdae816f05b449c0c1c41d42c34fe792a1a6c0c"
+    sha256 cellar: :any_skip_relocation, ventura:      "ac16aea87bedef54ba70303731aec056cb4b4c660db8f2514b5af703879c6d74"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "dc2c086c6e3f25deab84e85e8aa12e605b15482af437a976cd5d5071fec5df69"
   end
 
   depends_on "go" => [:build, :test]
