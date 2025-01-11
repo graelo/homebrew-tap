3dc19f70566481e6deb77218efb249ded23e0767
graelo <84066822+graelo@users.noreply.github.com>
2025-01-11 21:23:34 +0000

    jsonnet-language-server: update 0.15.0 bottle.

--- Formula/jsonnet-language-server.rb
+++ Formula/jsonnet-language-server.rb
@@ -6,10 +6,10 @@ class JsonnetLanguageServer < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    root_url "https://github.com/graelo/homebrew-tap/releases/download/jsonnet-language-server-0.14.1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "c9d2774bc1eba240fa53e3e1ffdae816f05b449c0c1c41d42c34fe792a1a6c0c"
-    sha256 cellar: :any_skip_relocation, ventura:      "ac16aea87bedef54ba70303731aec056cb4b4c660db8f2514b5af703879c6d74"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "dc2c086c6e3f25deab84e85e8aa12e605b15482af437a976cd5d5071fec5df69"
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/jsonnet-language-server-0.15.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "0a24e16a2a0d80a325c3ba3a71dc3e77c966431a18cc44f513bac5ff05e5c7bf"
+    sha256 cellar: :any_skip_relocation, ventura:      "019fcb5c45b68ba58b921376707a2be0bc4f4c1bfcd2f27d26eca265bd198e17"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "aa330e4beff7168f978387edc1f345806c85777b8949a50b20a279f9c2338b1e"
   end
 
   depends_on "go" => [:build, :test]
