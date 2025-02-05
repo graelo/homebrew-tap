856a8cc885bfc0dc82b8575cf86c972830dcf2d0
graelo <84066822+graelo@users.noreply.github.com>
2025-02-05 22:51:54 +0100

    dvoratt 0.1.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/dvoratt.rb
+++ Formula/dvoratt.rb
@@ -1,8 +1,8 @@
 class Dvoratt < Formula
   desc "Dvorak Typing Practice Terminal App"
   homepage "https://github.com/graelo/dvoratt"
-  url "https://github.com/graelo/dvoratt/archive/refs/tags/v0.0.0.tar.gz"
-  sha256 "60918deeb68a9f0b614581acc788b9aca720696f1cefebe2922b253e4c11f46f"
+  url "https://github.com/graelo/dvoratt/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "eb814ecff8450b83e08f674f3c9ea45f44de34c1ba8cce005e24f987bdf5bba1"
   license "MIT"
 
   depends_on "rust" => [:build, :test]
