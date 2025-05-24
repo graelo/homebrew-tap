ebffddfae97e288a950cb1689cd5adba94215d6b
graelo <84066822+graelo@users.noreply.github.com>
2025-05-24 22:00:22 +0000

    pumas: update 0.2.5 bottle.

--- Formula/pumas.rb
+++ Formula/pumas.rb
@@ -6,8 +6,10 @@ class Pumas < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/graelo/homebrew-tap/releases/download/pumas-0.2.4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "009de46a95cb625ad1e634bbbf36fc1ba0173bf1755fe6ef716eff27eec7a4be"
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/pumas-0.2.5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "c87ab37b6b9dfacbbce3fb3c9289fbc04cc37948f2bef6d691b9719588dd95d1"
+    sha256 cellar: :any_skip_relocation, ventura:      "fd2feaaf422a76d99c2cc70392f8bf1d94c90e8d793826eb583b99c05d70dbd7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f4218a3f1c2a633230dcd83dc0cab4c28e03d6a43f2e8ffecb08ec49614bd5dd"
   end
 
   depends_on "rust" => [:build, :test]
