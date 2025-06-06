3683e548a52b7fd25cc42017cff5fccae49f6629
graelo <84066822+graelo@users.noreply.github.com>
2025-06-06 22:25:39 +0000

    podfeed: update 0.3.0 bottle.

--- Formula/podfeed.rb
+++ Formula/podfeed.rb
@@ -6,10 +6,10 @@ class Podfeed < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/graelo/homebrew-tap/releases/download/podfeed-0.2.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "86d4a233e7573d579ceb99ee29d7c2ae78990171e88a4609746dafe31516b89e"
-    sha256 cellar: :any_skip_relocation, ventura:      "7b14dbc88c8188b6e1f066415e01521db7832fe4bf1a8f970e7d3683adb15152"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "2fcebe40d2e1e6ca4d23b6aa9cf40e0b4cebcccc5e65fa54724e98877b114777"
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/podfeed-0.3.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "31d5a6627b55f10282a44ce8e2d2610c2d1d6ffaf975706e521df22effa8f394"
+    sha256 cellar: :any_skip_relocation, ventura:      "54735e5f3d500f5a182f67fb017ae3182e296df4d70f7fd9ab756d2d8e2efc19"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "327cfdd2b336bd4dccf93b6124e24632f9b389ee1e723e3cdf5021e5c056b77a"
   end
 
   depends_on "rust" => [:build, :test]
