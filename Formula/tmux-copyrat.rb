88e3399da1d9d2c9760c11ae452ae7c9c1712bb1
graelo <84066822+graelo@users.noreply.github.com>
2025-11-23 19:24:29 +0000

    tmux-copyrat: update 0.6.1 bottle.

--- Formula/tmux-copyrat.rb
+++ Formula/tmux-copyrat.rb
@@ -6,9 +6,9 @@ class TmuxCopyrat < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/graelo/homebrew-tap/releases/download/tmux-copyrat-0.6.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ee03987ae9039318cb52802c97b3959ff7d9ea15856d501d4b81fa5c2ce90a27"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ce09d98f5a7b968fc2545e34ea46d84848e248a3773c221954b6634cfe35e01"
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/tmux-copyrat-0.6.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8746212c9e4e2b06402e582a22819a430b5bcb2468db1bcd59f08f82fbad5d55"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e172812abc9098002a54252687cbf5384c7918dff8df0b1fc27afaaac630dcf3"
   end
 
   depends_on "rust" => [:build, :test]
