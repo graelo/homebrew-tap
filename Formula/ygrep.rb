ff9abee14efdb9e658a3efa290d0c2ee3131ed63
graelo <84066822+graelo@users.noreply.github.com>
2026-02-11 09:36:57 +0000

    ygrep: update 2.0.4 bottle.

--- Formula/ygrep.rb
+++ Formula/ygrep.rb
@@ -6,9 +6,9 @@ class Ygrep < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/graelo/homebrew-tap/releases/download/ygrep-2.0.3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5ff47699109a59546303f3241c4128f9df05631cf3fe98ca56055f3e07b51f73"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "62489c06a9897fbe6ccfe7be9df5072748494964d0078dd745f4e773b86bae8b"
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/ygrep-2.0.4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ed29a167905c42ae0b3d2e7042a854ddc85198bfa180b39c7ec9f9f6171a294"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a48e3ca9af06894995f00cff191da3e628b25fe2030ae5019d5e8611ac17f89b"
   end
 
   depends_on "rust" => :build
