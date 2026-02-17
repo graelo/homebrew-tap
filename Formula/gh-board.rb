653982d85e2cabcb98aaeb97179898a0ad24ebfe
graelo <84066822+graelo@users.noreply.github.com>
2026-02-17 08:06:54 +0100

    gh-board 0.1.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/gh-board.rb
+++ Formula/gh-board.rb
@@ -1,8 +1,8 @@
 class GhBoard < Formula
   desc ""
   homepage "https://github.com/graelo/gh-board"
-  url "https://github.com/graelo/gh-board/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "07875b7b18c67ef89731b500effa96f365e056f609bf1058788fb8b445700f9a"
+  url "https://github.com/graelo/gh-board/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "07875b7b18c67ef89731b500effa96f365e056f609bf1058788fb8b339700f9a"
   license ""
 
   depends_on "rust" => :build
