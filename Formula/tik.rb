3c9cbc32b5a7831563c1777502d99ff82fce2515
graelo <84066822+graelo@users.noreply.github.com>
2026-03-22 00:27:10 +0100

    tik 0.1.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #144.
    
    Signed-off-by: graelo <84066822+graelo@users.noreply.github.com>

--- Formula/tik.rb
+++ Formula/tik.rb
@@ -1,8 +1,8 @@
 class Tik < Formula
   desc "Count LLM tokens in text files"
   homepage "https://github.com/graelo/tik"
-  url "https://github.com/graelo/tik/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "PLACEHOLDER"
+  url "https://github.com/graelo/tik/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "fecba41e18ab57295ec1c1c8612b77b446b39469e5b44cbaf7ff2a705312287c"
   license "MIT"
 
   depends_on "rust" => [:build, :test]
