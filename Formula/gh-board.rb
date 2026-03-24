fe0076b547948cea7e43ad3313ad686bb129a034
graelo <84066822+graelo@users.noreply.github.com>
2026-03-24 06:32:50 +0000

    gh-board: update 0.10.0 bottle.

--- Formula/gh-board.rb
+++ Formula/gh-board.rb
@@ -6,10 +6,10 @@ class GhBoard < Formula
   license ""
 
   bottle do
-    root_url "https://github.com/graelo/homebrew-tap/releases/download/gh-board-0.9.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "040b03bd9a6f68d74f9873d1062a42dfdeb4578280fbd873b4e421f2af3b7b08"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "baf8dc35f768044a8b1d23bdfa38a1458855f28b29b827ec3315e918760bc585"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "941e7b86c45daf3e3be9b571d9ff60ea8cb073ecf9c850cf2cfe9d73472a8044"
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/gh-board-0.10.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f6e46575f24e5ea8f3aa5eb5c596da477f0cb9a5f4471d23428f6e8ed4115ecb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ba30cae0b659f5b58ea063b4efcc6c093b7a002d44b2abed29f0e51abb3bcfbf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f8a59376520320b57c4565cf2462ac5df4a1e626b2f76e9ac0f56f734b2755af"
   end
 
   depends_on "rust" => :build
