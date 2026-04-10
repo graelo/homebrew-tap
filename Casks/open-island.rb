8ced312e47ada3e4b77f9830f4fbf366f2ebd5a4
graelo <graelo@graelo.cc>
2026-04-10 15:33:41 +0200

    open-island 1.0.14

---
+++ Casks/open-island.rb
@@ -0,0 +1,22 @@
+cask "open-island" do
+  version "1.0.14"
+  sha256 "52d7f15b36184a672669469fbc82c272bb5ab174d0291bc1f8ff9444a76839db"
+
+  url "https://github.com/Octane0411/open-vibe-island/releases/download/v#{version}/Open.Island.dmg"
+  name "Open Island"
+  desc "Open-source dynamic island for AI coding agents"
+  homepage "https://github.com/Octane0411/open-vibe-island"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Open Island.app"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  zap trash: "~/Library/Application Support/Open Island"
+end
