44e161faab5d74cfe13257fb1b609a0d215c2ff4
graelo <84066822+graelo@users.noreply.github.com>
2026-04-20 06:25:28 +0000

    dvoratt: update 0.1.5 bottle.

--- Formula/dvoratt.rb
+++ Formula/dvoratt.rb
@@ -6,10 +6,10 @@ class Dvoratt < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/graelo/homebrew-tap/releases/download/dvoratt-0.1.4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "73753b1c9f7e7b0199754f96baf5af0614573de7b7264776f0b81bbcae638e52"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "863c6b2ef3238468fd9b0e40b11a542cd30229ffb72b01ce833b62c7340a1d94"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "899e97b482f8e7c42ffa895be7c89bc84b1ab537d304268ac5461dba2b798e93"
+    root_url "https://github.com/graelo/homebrew-tap/releases/download/dvoratt-0.1.5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "38304235f98dc4baa360f0a6f5d2b5408f13484c2b34ed8dda2366d2e8dfe195"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "112cd01570b3b563daf912e9af69a801ea9e1408885522a1224081b3ffcdb373"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "912f7421cd0cb4f897000e04b3505f343b40caec2ca372ad3cea672d9141cf5d"
   end
 
   depends_on "rust" => [:build, :test]
